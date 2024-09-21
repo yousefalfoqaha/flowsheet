6761a5bd9b77cdc2b3c2d024f8f464e92c1d8083
yousefalfoqaha <yousefmustafaadam@gmail.com>
2024-09-21 11:49:47 +0300

    added shadcn components, split flowsheet into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Draft } from 'immer'
 import { useImmerReducer } from 'use-immer'
+import { Button } from './components/ui/button'
 
 type Flowsheet = {
   id: number
@@ -34,11 +35,28 @@ const initialFlowsheet: flowsheetState = {
   years: {
     1: {
       id: 1,
-      semesterIds: [],
+      semesterIds: [1, 2],
+    },
+    2: {
+      id: 2,
+      semesterIds: [3],
     },
   },
-  semesters: {},
-  addedCourses: [],
+  semesters: {
+    1: {
+      id: 1,
+      courseIds: [16, 17],
+    },
+    2: {
+      id: 2,
+      courseIds: [20, 21],
+    },
+    3: {
+      id: 3,
+      courseIds: [18, 19],
+    },
+  },
+  addedCourses: [16, 17, 20, 21, 18, 19],
   error: null,
 }
 
@@ -125,55 +143,105 @@ export default function App() {
     initialFlowsheet
   )
 
+  return <Flowsheet flowsheet={flowsheet} dispatch={dispatch} />
+}
+
+function Flowsheet({
+  flowsheet,
+  dispatch,
+}: {
+  flowsheet: flowsheetState
+  dispatch: any
+}) {
+  return (
+    <div className="flex gap-1">
+      {Object.values(flowsheet.years).map((year: Year) => {
+        if (!year) return null
+        return (
+          <ul key={year.id}>
+            <Year
+              id={year.id}
+              semesterIds={year.semesterIds}
+              flowsheet={flowsheet}
+              dispatch={dispatch}
+            />
+          </ul>
+        )
+      })}
+    </div>
+  )
+}
+
+function Year({
+  id,
+  semesterIds,
+  flowsheet,
+  dispatch,
+}: Year & { flowsheet: flowsheetState; dispatch: any }) {
+  return (
+    <section>
+      <header className="bg-zinc-200 px-10 text-center">Year {id}</header>
+      <ul className="flex gap-1">
+        {semesterIds.map((id) => {
+          const semester = flowsheet.semesters[id]
+          if (!semester) return null
+          return (
+            <li key={id}>
+              <Semester
+                id={id}
+                courseIds={semester.courseIds}
+                dispatch={dispatch}
+              />
+            </li>
+          )
+        })}
+      </ul>
+    </section>
+  )
+}
+
+function Semester({ id, courseIds, dispatch }: Semester & { dispatch: any }) {
   const handleAddCourse = (semesterId: number, courseId: number) =>
     dispatch({ type: 'ADD_COURSE', payload: { semesterId, courseId } })
 
   return (
-    <div className="flex justify-center h-full">
-      {Object.values(flowsheet.years).map((year: Year) => (
-        <section key={year.id}>
-          <header>Year {year.id}</header>
-          <div className="flex gap-1">
-            {year.semesterIds.map((id) => {
-              const semester = flowsheet.semesters[id]
-              if (!semester) return null
-              return (
-                <section key={semester.id} className="m-5">
-                  <header className="font-semibold text-center">
-                    Semester {semester.id}
-                  </header>
-                  <div className="flex flex-col items-center">
-                    {semester.courseIds.map((id) => {
-                      const course = courses[id]
-                      if (!course) return null
-                      return (
-                        <div
-                          key={course.id}
-                          className="p-2 h-36 w-36 transition-all border border-neutral-300 hover:shadow rounded-md flex flex-col bg-neutral-200 hover:bg-purple-400/30"
-                        >
-                          <header className="font-semibold">
-                            {course.code}
-                          </header>
-                          <p className="line-clamp-3">{course.name}</p>
-                          <footer className="text-right mt-auto text-neutral-500">
-                            {course.creditHours} Cr Hr
-                          </footer>
-                        </div>
-                      )
-                    })}
-                    <button
-                      onClick={() => handleAddCourse(semester.id, 16)}
-                      className="mt-1 hover:bg-purple-400/50 rounded p-2 border min-w-28 border-zinc-300 w-full transition-all"
-                    >
-                      +
-                    </button>
-                  </div>
-                </section>
-              )
-            })}
-          </div>
-        </section>
-      ))}
+    <section className="flex flex-col gap-1">
+      <header className="text-center">Semester {id}</header>
+      <ul className="flex flex-col gap-1">
+        {courseIds.map((id) => {
+          const course = courses[id]
+          if (!course) return null
+          return (
+            <li key={id}>
+              <Course
+                id={id}
+                code={course.code}
+                name={course.name}
+                creditHours={course.creditHours}
+              />
+            </li>
+          )
+        })}
+      </ul>
+      <Button onClick={() => handleAddCourse(id, 22)} variant="outline">
+        Add course
+      </Button>
+    </section>
+  )
+}
+
+function Course({ code, name, creditHours }: Course) {
+  return (
+    <div className="border rounded transition-all hover:bg-blue-100 cursor-pointer flex flex-col p-3 bg-zinc-200 w-36 h-36">
+      <header className="text-left flex gap-2 w-full">
+        <h1 className="font-semibold">{code}</h1>
+      </header>
+      <p className="whitespace-normal font-normal text-sm overflow-hidden text-ellipsis line-clamp-3">
+        {name}
+      </p>
+      <footer className="w-full mt-auto flex text-xs font-semibold text-muted-foreground">
+        <p className="ml-auto">{creditHours} Cr Hr</p>
+      </footer>
     </div>
   )
 }
