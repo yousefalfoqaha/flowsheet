c2680bd766b23f505656d041d7ff69aa6a248163
Yousef Mstfa <yousefmustafaadam@gmail.com>
2024-09-30 21:27:54 +0300

    study plan modal gui overhaul

--- src/components/Course.tsx
+++ src/components/Course.tsx
@@ -1,12 +1,13 @@
-import { Plus, ArrowLeftFromLine, Trash } from 'lucide-react'
+import { Plus, Trash, Check, Info } from 'lucide-react'
 import { Button } from './ui/button'
+import { Badge } from './ui/badge'
 
 type CourseProps = {
   code: string
   name: string
   creditHours: number
   status: string
-  onClick: () => void
+  onClick?: () => void
 }
 
 export function Course({
@@ -16,51 +17,48 @@ export function Course({
   status,
   onClick,
 }: CourseProps) {
-  let height = 'h-full'
-  let hoverColor = ''
-  let icon = null
-
-  switch (status) {
-    case 'STUDY_PLAN':
-      height = 'h-24'
-      hoverColor = 'hover:bg-green-500/50'
-      icon = <Plus className="scale-90" />
-      break
-    case 'PENDING':
-      height = 'h-36'
-      hoverColor = 'hover:bg-blue-300/50'
-      icon = <ArrowLeftFromLine className="scale-90" />
-      break
-    case 'FLOWSHEET':
-      height = 'h-36'
-      hoverColor = 'hover:bg-red-500/50'
-      icon = <Trash className="scale-90" />
-      break
-    default:
-      height = 'h-full'
-      hoverColor = ''
-      icon = null
-      break
-  }
-
-  return (
-    <Button
-      variant="ghost"
-      onClick={onClick}
-      className={`group text-left flex flex-col p-3 bg-zinc-200 w-full ${height} rounded-md relative ${hoverColor}`} // apply hoverColor class here
-    >
-      <header className="w-full font-semibold">{code}</header>
-      <p
-        className={`w-full whitespace-normal font-normal text-sm overflow-hidden text-ellipsis line-clamp-3`}
-      >
-        {name}
-      </p>
-      <footer className="mt-auto ml-auto flex text-xs font-semibold text-muted-foreground">
-        {creditHours} Cr Hr
-      </footer>
-      <div className="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-all">
-        {icon}
+  if (status === 'STUDY_PLAN')
+    return (
+      <div className="group flex flex-row w-full p-3 border-b gap-2">
+        <div className="flex flex-col gap-2 w-full">
+          <div className="flex gap-2">
+            <p className="font-semibold my-auto border-r pr-2">{code}</p>
+            <Badge variant="outline" className="w-fit">
+              {creditHours} Cr Hr
+            </Badge>
+          </div>
+          <p>{name}</p>
+        </div>
+        <div className="flex gap-1 transition-all ml-auto my-auto">
+          <Button variant="ghost" className="my-auto w-13">
+            <Info className="scale-75" />
+          </Button>
+          <Button onClick={onClick}>Add</Button>
+        </div>
       </div>
-    </Button>
-  )
+    )
+
+  if (status === 'FLOWSHEET')
+    return (
+      <Button
+        variant="ghost"
+        onClick={onClick}
+        className="group text-left flex flex-col p-3 w-full h-36 rounded-md relative"
+      >
+        <header className="w-full font-semibold">{code}</header>
+        <p
+          className={`w-full whitespace-normal font-normal text-sm overflow-hidden text-ellipsis line-clamp-3`}
+        >
+          {name}
+        </p>
+        <footer className="mt-auto ml-auto flex text-xs font-semibold text-muted-foreground">
+          {creditHours} Cr Hr
+        </footer>
+        <div className="absolute top-2 right-2">
+          {/* <div className="opacity-0 group-hover:opacity-100 transition-all">
+          {icon}
+        </div> */}
+        </div>
+      </Button>
+    )
 }
--- src/components/StudyPlan.tsx
+++ src/components/StudyPlan.tsx
@@ -1,13 +1,26 @@
-import { courses } from '@/data/courses'
 import {
   Dialog,
   DialogHeader,
   DialogFooter,
   DialogContent,
   DialogTitle,
+  DialogClose,
+  DialogDescription,
 } from './ui/dialog'
-import { DataTable } from './courses-table/DataTable'
-import { columns } from './courses-table/columns'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from './ui/accordion'
+import { Section, sections } from '@/data/sections'
+import { courses, Course } from '@/data/courses'
+import { Course as SectionCourse } from './Course'
+import React from 'react'
+import { Button } from './ui/button'
+import { Badge } from './ui/badge'
+import { useSemesters } from '@/hooks/useSemesters'
+import { X } from 'lucide-react'
 
 type StudyPlanProps = {
   semesterId: number
@@ -15,28 +28,125 @@ type StudyPlanProps = {
 }
 
 export function StudyPlan({ semesterId, onCloseStudyPlan }: StudyPlanProps) {
+  const [selectedCourses, setSelectedCourses] = React.useState<Course[]>([])
+  const { addCourseToSemester } = useSemesters()
+
+  const handleSelectCourse = (clickedCourse: Course) => {
+    setSelectedCourses((prev) =>
+      prev.includes(clickedCourse)
+        ? prev.filter((course) => course.id !== clickedCourse.id)
+        : [...prev, clickedCourse]
+    )
+  }
   return (
     <Dialog
       open={!!semesterId}
       onOpenChange={() => {
         onCloseStudyPlan()
+        setSelectedCourses([])
       }}
     >
-      <DialogContent className="min-w-[60rem] h-[50rem] flex flex-col">
+      <DialogContent className="h-screen flex flex-col">
         <DialogHeader>
-          <DialogTitle className='text-xl'>
+          <DialogTitle className="text-xl">
             Bachelor in Computer Science 2023/2024 Courses
           </DialogTitle>
+          <DialogDescription>
+            Browse sections to add courses to the semester
+          </DialogDescription>
         </DialogHeader>
-        <div className="flex-grow overflow-hidden">
-          <DataTable
-            columns={columns}
-            data={Object.values(courses)}
-            semesterId={semesterId}
-            onCloseStudyPlan={onCloseStudyPlan}
-          />
+        <div className="overflow-y-auto h-full">
+          <Accordion type="single" collapsible className="mx-1">
+            {Object.values(sections).map((section: Section) => {
+              return (
+                <AccordionItem value={`${section.id}`} className="border-none">
+                  <AccordionTrigger className="font-normal hover:bg-zinc-50 transition-all rounded-xl px-3 hover:shadow-md">
+                    <div className="flex flex-col text-left">
+                      <p>{section.name}</p>
+                      <p className="text-sm text-muted-foreground">
+                        {section.requiredCreditHours} Cr Hr Remaining
+                      </p>
+                    </div>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="flex flex-col mt-2">
+                      {section.courseIds.map((id) => {
+                        const course = courses[id]
+                        if (!course) return
+                        let status = 'STUDY_PLAN'
+                        if (selectedCourses.includes(course)) status = 'PENDING'
+                        return (
+                          <SectionCourse
+                            code={course.code}
+                            name={course.name}
+                            creditHours={course.creditHours}
+                            status={status}
+                            onClick={() => handleSelectCourse(course)}
+                          />
+                        )
+                      })}
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              )
+            })}
+          </Accordion>
         </div>
-        <DialogFooter className="mt-4"></DialogFooter>
+
+        <DialogFooter className="mt-auto pt-2 border-t">
+          <div className="flex flex-col gap-1 w-full">
+            {selectedCourses.length !== 0 ? (
+              <>
+                <div className="flex flex-col gap-2 w-full">
+                  <p>{selectedCourses.length} Course(s) selected:</p>
+                  <div className=" flex flex-wrap gap-1">
+                    {selectedCourses.map((course) => {
+                      return (
+                        <Badge
+                          variant="secondary"
+                          onClick={() => handleSelectCourse(course)}
+                          className="cursor-pointer hover:bg-red-400/50 h-6"
+                        >
+                          {course.name}
+                        </Badge>
+                      )
+                    })}
+                  </div>
+                </div>
+                <div className="flex flex-col gap-1">
+                  <Button
+                    onClick={() => setSelectedCourses([])}
+                    variant="ghost"
+                  >
+                    <>
+                      <X className="scale-75" />
+                      <p>Clear selection</p>
+                    </>
+                  </Button>
+                  <DialogClose asChild>
+                    <Button
+                      onClick={() =>
+                        selectedCourses.map((course: Course) =>
+                          addCourseToSemester(semesterId, course.id)
+                        )
+                      }
+                      className=" mt-auto"
+                    >
+                      Add courses
+                    </Button>
+                  </DialogClose>
+                </div>
+              </>
+            ) : (
+              <div className="flex flex-col gap-3">
+                <p className="text-muted-foreground my-auto text-center">
+                  No courses selected.
+                </p>
+                <Button disabled>Add courses</Button>
+              </div>
+            )}
+          </div>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   )
