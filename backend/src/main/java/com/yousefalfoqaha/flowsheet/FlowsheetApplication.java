ccb219ef46020e0bafcd8ebdecdabc82043afb68
yousefalfoqaha <yousefmustafaadam@gmail.com>
2024-10-12 10:00:41 +0300

    added springboot backend to project, reorganized into frontend and backend monorepos

---
+++ backend/src/main/java/com/yousefalfoqaha/flowsheet/FlowsheetApplication.java
@@ -0,0 +1,13 @@
+package com.yousefalfoqaha.flowsheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowsheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowsheetApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yousefalfoqaha/flowsheet/FlowsheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yousefalfoqaha.flowsheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowsheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
