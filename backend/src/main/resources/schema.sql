47c07b6e1a1cd5c483442025b5ffc7a23673bcee
yousefalfoqaha <yousefmustafaadam@gmail.com>
2024-12-16 21:46:47 +0300

    co requisite table for courses added, course details added for data entry

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -5,16 +5,20 @@ DROP TABLE IF EXISTS section CASCADE;
 DROP TABLE IF EXISTS course_prerequisite CASCADE;
 DROP TABLE IF EXISTS course CASCADE;
 DROP TABLE IF EXISTS program CASCADE;
+DROP TABLE IF EXISTS course_details CASCADE;
+DROP TABLE IF EXISTS course_corequisite CASCADE;
 
-DROP TYPE IF EXISTS enum_degree CASCADE;
-DROP TYPE IF EXISTS enum_section_level CASCADE;
-DROP TYPE IF EXISTS enum_section_type CASCADE;
-DROP TYPE IF EXISTS enum_relation CASCADE;
+DROP TYPE IF EXISTS degree CASCADE;
+DROP TYPE IF EXISTS section_level CASCADE;
+DROP TYPE IF EXISTS section_type CASCADE;
+DROP TYPE IF EXISTS relation CASCADE;
+DROP TYPE IF EXISTS teaching_method CASCADE;
 
-CREATE TYPE enum_degree AS ENUM ('BACHELOR', 'MASTER');
-CREATE TYPE enum_section_level AS ENUM ('UNIVERSITY', 'SCHOOL', 'PROGRAM');
-CREATE TYPE enum_section_type AS ENUM ('REQUIREMENT', 'ELECTIVE', 'REMEDIAL');
-CREATE TYPE enum_relation AS ENUM ('AND', 'OR');
+CREATE TYPE degree AS ENUM ('BACHELOR', 'MASTER');
+CREATE TYPE section_level AS ENUM ('UNIVERSITY', 'SCHOOL', 'PROGRAM');
+CREATE TYPE section_type AS ENUM ('REQUIREMENT', 'ELECTIVE', 'REMEDIAL');
+CREATE TYPE relation AS ENUM ('AND', 'OR');
+CREATE TYPE teaching_method AS ENUM ('Face-to-face', 'Blended', 'Online');
 
 CREATE TABLE course (
     id SERIAL PRIMARY KEY,
@@ -23,21 +27,39 @@ CREATE TABLE course (
     credit_hours INT NOT NULL
 );
 
+CREATE TABLE course_details (
+    course INT PRIMARY KEY,
+    lecture_hours INT NOT NULL,
+    practical_hours INT NOT NULL,
+    teaching_method teaching_method NOT NULL,
+    ects INT NOT NULL,
+    description TEXT,
+    FOREIGN KEY (course) REFERENCES course(id)
+);
+
 CREATE TABLE program (
     id SERIAL PRIMARY KEY,
     code VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    degree enum_degree NOT NULL,
+    degree degree NOT NULL,
     UNIQUE (code, degree)
 );
 
 CREATE TABLE course_prerequisite (
     course INT NOT NULL,
     prerequisite INT NOT NULL,
-    relation enum_relation NOT NULL,
+    relation relation NOT NULL,
     PRIMARY KEY (course, prerequisite),
     FOREIGN KEY (course) REFERENCES course(id) ON DELETE CASCADE,
     FOREIGN KEY (prerequisite) REFERENCES course(id) ON DELETE CASCADE
+);
+
+CREATE TABLE course_corequisite (
+	course INT NOT NULL,
+	corequisite INT NOT NULL,
+	PRIMARY KEY (course, corequisite),
+	FOREIGN KEY (course) REFERENCES course(id) ON DELETE CASCADE,
+	FOREIGN KEY (corequisite) REFERENCES course(id) ON DELETE CASCADE
 );
 
 CREATE TABLE study_plan (
@@ -57,8 +79,8 @@ CREATE TABLE track (
 
 CREATE TABLE section (
     id SERIAL PRIMARY KEY,
-    level enum_section_level NOT NULL,
-    type enum_section_type NOT NULL,
+    level section_level NOT NULL,
+    type section_type NOT NULL,
     required_credit_hours INT NOT NULL,
     name VARCHAR(255),
     study_plan INT NOT NULL,
